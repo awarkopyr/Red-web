218b7df5eef1f519f98302fc30d8704e46f64fd1
awarkopyr <awarkohm2002@gmail.com>
2025-09-19 04:15:46 +0600

    update

--- src/assets/asset.js
+++ src/assets/asset.js
@@ -1,4 +1,4 @@
-import logo1 from './logo1.png'
+
 import one from './one.jpg'
 import two from './two.jpg'
 import three from './three.jpg'
@@ -7,7 +7,6 @@ import five from './five.jpg'
 import six from './six.jpg'
 import seven from './seven.jpg'
 import eight from './eight.jpg'
-import logo2 from './logo2.png'
 import owner from './owner.png'
 
-export{logo1,one,two,three,four,five,six,seven,eight, logo2, owner}
+export{one,two,three,four,five,six,seven,eight, owner}
